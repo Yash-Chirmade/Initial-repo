b0a0088d432e966fcc49499d6ce0afbda986621e
Yash <sea.yash29@gmail.com>
2021-03-22 13:58:59 +0530

    initial commit

---
+++ practiceques.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if (a>b){
+        if (a>c){
+            cout<<"a is biggest"<<endl;
+
+        }
+    }else if (b>c)
+
+    {
+        cout<<"b is biggest"<<endl;
+
+    }else {
+        cout<<"c is biggest"<<endl;
+    }
+    
+    return 0;
+
+
+}
+    
